3924b4ca181a642e6ea1f54cfa56a6efba6897ae
Joven Tan <jovenntann@gmail.com>
2024-04-16 01:04:54 +0800

    feat(facebook auth): Add Facebook login functionality

---
+++ pages/facebook.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="login">Login with Facebook</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      FB.login(function(response) {
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+          });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+      });
+    }
+  },
+  mounted() {
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    // Add your App ID at the end of the following line
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0&appId=817039849052260";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+}
+}
+</script>
+
